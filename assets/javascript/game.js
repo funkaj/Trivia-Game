c89b271b073949165d898a50660a355df4a1e224
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-06-09 23:15:53 -0700

    created working branch and psudocode for project

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// document ready
+
+// assign varibles 
+let right = 0;
+let wrong = 0;
+let countdownTimer = 0;
+// create object array of questions and answers
+let questions = [
+    {}
+]
+
+//countdown timer
+
+//new game function
+
+//win function should hide other screens for a few seconds then load new game
+
+//lose function will display correct answer wait a few seconds and show next question
+
+//game function loops to get and display questions and user input start/end timer and redirect to win or lose function
+
+//function to display user stats when all questions are answered with a restart button
