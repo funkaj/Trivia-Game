3faa6c9cdd8c63fd8f798fdf4247e26572ca973e
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-06-16 00:53:21 -0700

    as good as it gets

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,119 +12,115 @@ $(document).ready(function () {
         newGame();
     })
 
-   
-
     // create object array of questions and answers
     let questions = [{
-            question: "What was Mega-Man's original Japanese name?",
-            answers: [{
-                    name: "Rock-man",
-                    isCorrect: 1
-                },
-                {
-                    name: "Blue-Man",
-                    isCorrect: 0
-                },
-                {
-                    name: "Man-Man",
-                    isCorrect: 0
-                },
-                {
-                    name: "Zero",
-                    isCorrect: 0
-                }
-            ],
-            image: "assets/images/rockMan.jpeg"
+        question: "What was Mega-Man's original Japanese name?",
+        answers: [{
+            name: "Rock-man",
+            isCorrect: 1
         },
         {
-            question: "Who is the creator of Mega-Man in the game story?",
-            answers: [{
-                    name: "Dr. Wily",
-                    isCorrect: 0
-                },
-                {
-                    name: "Dr. Who",
-                    isCorrect: 0
-                },
-                {
-                    name: "Dr. Suess",
-                    isCorrect: 0
-                },
-                {
-                    name: "Dr. Light",
-                    isCorrect: 1
-                }
-            ],
-            image: "assets/images/drLight.jpeg"
+            name: "Blue-Man",
+            isCorrect: 0
         },
         {
-            question: "Who is Mega-Man trying to save the world from?",
-            answers: [{
-                    name: "Dr. Doom",
-                    isCorrect: 0
-                },
-                {
-                    name: "Dr. Wily",
-                    isCorrect: 1
-                },
-                {
-                    name: "Billy",
-                    isCorrect: 0
-                },
-                {
-                    name: "Dr. Evil",
-                    isCorrect: 0
-                }
-            ],
-            image: "assets/images/drWily.jpeg"
+            name: "Man-Man",
+            isCorrect: 0
         },
         {
-            question: "Who is Mega-Man's brother?",
-            answers: [{
-                    name: "Proto-Man",
-                    isCorrect: 1
-                },
-                {
-                    name: "Zero",
-                    isCorrect: 0
-                },
-                {
-                    name: "Rush",
-                    isCorrect: 0
-                },
-                {
-                    name: "Astro-Boy",
-                    isCorrect: 0
-                }
-            ],
-            image: "assets/images/protoMan.jpeg"
-        },
-        {
-            question: "What is Mega-Man's sisters name?",
-            answers: [{
-                    name: "Roll",
-                    isCorrect: 1
-                },
-                {
-                    name: "Mega-Woman",
-                    isCorrect: 0
-                },
-                {
-                    name: "Robot-Girl",
-                    isCorrect: 0
-                },
-                {
-                    name: "Vicki",
-                    isCorrect: 0
-                }
-            ],
-            image: "assets/images/roll.jpeg"
+            name: "Zero",
+            isCorrect: 0
         }
+        ],
+        image: "assets/images/rockMan.jpeg"
+    },
+    {
+        question: "Who is the creator of Mega-Man in the game story?",
+        answers: [{
+            name: "Dr. Wily",
+            isCorrect: 0
+        },
+        {
+            name: "Dr. Who",
+            isCorrect: 0
+        },
+        {
+            name: "Dr. Suess",
+            isCorrect: 0
+        },
+        {
+            name: "Dr. Light",
+            isCorrect: 1
+        }
+        ],
+        image: "assets/images/drLight.jpeg"
+    },
+    {
+        question: "Who is Mega-Man trying to save the world from?",
+        answers: [{
+            name: "Dr. Doom",
+            isCorrect: 0
+        },
+        {
+            name: "Dr. Wily",
+            isCorrect: 1
+        },
+        {
+            name: "Billy",
+            isCorrect: 0
+        },
+        {
+            name: "Dr. Evil",
+            isCorrect: 0
+        }
+        ],
+        image: "assets/images/drWily.jpeg"
+    },
+    {
+        question: "Who is Mega-Man's brother?",
+        answers: [{
+            name: "Proto-Man",
+            isCorrect: 1
+        },
+        {
+            name: "Zero",
+            isCorrect: 0
+        },
+        {
+            name: "Rush",
+            isCorrect: 0
+        },
+        {
+            name: "Astro-Boy",
+            isCorrect: 0
+        }
+        ],
+        image: "assets/images/protoMan.jpeg"
+    },
+    {
+        question: "What is Mega-Man's sisters name?",
+        answers: [{
+            name: "Roll",
+            isCorrect: 1
+        },
+        {
+            name: "Mega-Woman",
+            isCorrect: 0
+        },
+        {
+            name: "Robot-Girl",
+            isCorrect: 0
+        },
+        {
+            name: "Vicki",
+            isCorrect: 0
+        }
+        ],
+        image: "assets/images/roll.jpeg"
+    }
     ]
 
-    
-
-    //countdown timer
+    //countdown timer needs to be fixed
     function timer() {
         var timeLeft = 10;
         var guessTimer = setInterval(function () {
@@ -135,20 +131,18 @@ $(document).ready(function () {
                 userWrong()
                 timeLeft = 10
             }
-
         }, 1000);
-
     }
 
     //game function loops to get and display questions and user input start/end timer and redirect to win or lose function
     function game() {
-      
+
+        // timer() doesnt work yet
+        //tried creating an array so the questions are always in a different order. kept getting duplicates. this does too unfortunatly.
         let currentQuestion = questions[Math.floor(Math.random() * questions.length)];
         console.log(currentQuestion)
         $('.questions').text('Question: ' + currentQuestion.question)
-        let questionAnswers = currentQuestion.answers //loop?
-        let newDivCounter = 1
-        
+        let questionAnswers = currentQuestion.answers
 
         for (var i = 0; i < currentQuestion.answers.length; i++) {
 
@@ -157,28 +151,34 @@ $(document).ready(function () {
             var displayAnswers = $('<div class="answers row"></div>')
             var answer = questionAnswers[i]
             var $newDivId = displayAnswers.append($(`<div class="btn col-12 mt-4" name="${currentQuestion.answers[i].name}" value="${$validate}">` + answer.name + `</div>`))
+
             $('#questionsField').append($newDivId)
         }
-            $('.btn').on('click', function () {
-                let guess = $(this).attr('value')
-                if (guess > 0) {
-                    console.log('Finally!')
-                    userCorrect();
-                } else {
-                    console.log('WRONG!')
-                    function correct() {
-                       //highlight correct answer
-                        userWrong();
-                      }
-                      setTimeout(correct, 3000);     
+        $('.btn').on('click', function () {
+            let guess = $(this).attr('value')
+            if (guess > 0) {
+                console.log('Finally!')
+                userCorrect();
+            } else {
+                on()
+                console.log('WRONG!')
+                function correct() {
+                    //highlight correct answer
+                    userWrong();
                 }
-            })
-
+                setTimeout(correct, 4000);
+            }
+        })
     }
-
+    function on() {
+        document.getElementById("overlay").style.display = "block";
+    }
+    function off() {
+        document.getElementById("overlay").style.display = "none";
+    }
     //new game function
     function newGame() {
-       
+
         rightAnswers = 0
         wrongAnswers = 0
         $('#winScreen').hide()
@@ -193,42 +193,41 @@ $(document).ready(function () {
     }
     //userCorrect function should hide other screens for a few seconds then load new game
     function userCorrect() {
-        $('#questionField').empty()
+
+        $('#questionsField').hide()
         rightAnswers++
         $('#rightAnswers').text(rightAnswers)
-        $('#questionField').hide()
         $('#winScreen').show()
         $('.answers').remove()
         if (rightAnswers + wrongAnswers < questions.length) {
             function correct() {
-            $('#winScreen').hide()
-            game()
-          }
-          setTimeout(correct, 1000);
+                $('#winScreen').hide()
+                $('#questionsField').show()
+                game()
+            }
+            setTimeout(correct, 2000);
         } else {
             statScreen()
         }
-        
-        
     }
     //userWrongTime function will display correct answer wait a few seconds and show next question
     function userWrong() {
-    
         wrongAnswers++
         $('#wrongAnswers').text(wrongAnswers)
         $('.answers').remove()
         if (rightAnswers + wrongAnswers < questions.length) {
-            function correct() {
+            off()
             game()
-          }
-          setTimeout(correct, 1000);
         } else {
+            off()
             statScreen()
         }
     }
     //function to display user stats when all questions are answered with a restart button
 
     function statScreen() {
+        $('#questionsField').hide()
+        $('#winScreen').hide()
         $('#statScreen').show()
         $('#rightAnswers').text(rightAnswers)
         $('#wrongAnswers').text(wrongAnswers)
