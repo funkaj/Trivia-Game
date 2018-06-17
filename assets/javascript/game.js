30a1bb32b012b32c3ff499bfd7941caed62f543d
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-06-17 00:57:45 -0700

    needs some polish

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 // document ready
 $(document).ready(function () {
-    var guessTimer = setInterval(function() {
+    var guessTimer = setInterval(function () {
         timerLoop()
-        }, 1000);
+    }, 1000);
     var timeLeft = 10;
     timerStop(guessTimer);
     $('.jumbotron').hide();
@@ -19,113 +19,112 @@ $(document).ready(function () {
 
     // create object array of questions and answers
     let questions = [{
-            question: "What was Mega-Man's original Japanese name?",
-            answers: [{
-                    name: "Rock-man",
-                    isCorrect: 1
-                },
-                {
-                    name: "Blue-Man",
-                    isCorrect: 0
-                },
-                {
-                    name: "Man-Man",
-                    isCorrect: 0
-                },
-                {
-                    name: "Zero",
-                    isCorrect: 0
-                }
-            ],
-            image: "assets/images/rockMan.jpeg"
+        question: "What was Mega-Man's original Japanese name?",
+        answers: [{
+            name: "Rock-man",
+            isCorrect: 1
         },
         {
-            question: "Who is the creator of Mega-Man in the game story?",
-            answers: [{
-                    name: "Dr. Wily",
-                    isCorrect: 0
-                },
-                {
-                    name: "Dr. Who",
-                    isCorrect: 0
-                },
-                {
-                    name: "Dr. Suess",
-                    isCorrect: 0
-                },
-                {
-                    name: "Dr. Light",
-                    isCorrect: 1
-                }
-            ],
-            image: "assets/images/drLight.jpeg"
+            name: "Blue-Man",
+            isCorrect: 0
         },
         {
-            question: "Who is Mega-Man trying to save the world from?",
-            answers: [{
-                    name: "Dr. Doom",
-                    isCorrect: 0
-                },
-                {
-                    name: "Dr. Wily",
-                    isCorrect: 1
-                },
-                {
-                    name: "Billy",
-                    isCorrect: 0
-                },
-                {
-                    name: "Dr. Evil",
-                    isCorrect: 0
-                }
-            ],
-            image: "assets/images/drWily.jpeg"
+            name: "Man-Man",
+            isCorrect: 0
         },
         {
-            question: "Who is Mega-Man's brother?",
-            answers: [{
-                    name: "Proto-Man",
-                    isCorrect: 1
-                },
-                {
-                    name: "Zero",
-                    isCorrect: 0
-                },
-                {
-                    name: "Rush",
-                    isCorrect: 0
-                },
-                {
-                    name: "Astro-Boy",
-                    isCorrect: 0
-                }
-            ],
-            image: "assets/images/protoMan.jpeg"
-        },
-        {
-            question: "What is Mega-Man's sisters name?",
-            answers: [{
-                    name: "Roll",
-                    isCorrect: 1
-                },
-                {
-                    name: "Mega-Woman",
-                    isCorrect: 0
-                },
-                {
-                    name: "Robot-Girl",
-                    isCorrect: 0
-                },
-                {
-                    name: "Vicki",
-                    isCorrect: 0
-                }
-            ],
-            image: "assets/images/roll.jpeg"
+            name: "Zero",
+            isCorrect: 0
         }
+        ],
+        image: "assets/images/rockMan.jpeg"
+    },
+    {
+        question: "Who is the creator of Mega-Man in the game story?",
+        answers: [{
+            name: "Dr. Wily",
+            isCorrect: 0
+        },
+        {
+            name: "Dr. Who",
+            isCorrect: 0
+        },
+        {
+            name: "Dr. Suess",
+            isCorrect: 0
+        },
+        {
+            name: "Dr. Light",
+            isCorrect: 1
+        }
+        ],
+        image: "assets/images/drLight.jpeg"
+    },
+    {
+        question: "Who is Mega-Man trying to save the world from?",
+        answers: [{
+            name: "Dr. Doom",
+            isCorrect: 0
+        },
+        {
+            name: "Dr. Wily",
+            isCorrect: 1
+        },
+        {
+            name: "Billy",
+            isCorrect: 0
+        },
+        {
+            name: "Dr. Evil",
+            isCorrect: 0
+        }
+        ],
+        image: "assets/images/drWily.jpeg"
+    },
+    {
+        question: "Who is Mega-Man's brother?",
+        answers: [{
+            name: "Proto-Man",
+            isCorrect: 1
+        },
+        {
+            name: "Zero",
+            isCorrect: 0
+        },
+        {
+            name: "Rush",
+            isCorrect: 0
+        },
+        {
+            name: "Astro-Boy",
+            isCorrect: 0
+        }
+        ],
+        image: "assets/images/protoMan.jpeg"
+    },
+    {
+        question: "What is Mega-Man's sisters name?",
+        answers: [{
+            name: "Roll",
+            isCorrect: 1
+        },
+        {
+            name: "Mega-Woman",
+            isCorrect: 0
+        },
+        {
+            name: "Robot-Girl",
+            isCorrect: 0
+        },
+        {
+            name: "Vicki",
+            isCorrect: 0
+        }
+        ],
+        image: "assets/images/roll.jpeg"
+    }
     ]
 
-    
     // countdown timer needs to be fixed
 
     var timerLoop = function () {
@@ -138,26 +137,16 @@ $(document).ready(function () {
             resetTimer();
         }
     };
-
-    //let tempQuestionArray = [];
-
-    // while (tempQuestionArray.length < questions.length) {
-    //     let currentQuestion = questions[Math.floor(Math.random() * questions.length)];
-    //     if (!tempQuestionArray.includes(currentQuestion)) {
-    //         tempQuestionArray.push(Number(currentQuestion));
-    //     }
-    // }
-    //console.log(tempQuestionArray)
     //game function loops to get and display questions and user input start/end timer and redirect to win or lose function
     function resetTimer() {
         timeLeft = 10
     }
-    
+
     function game() {
         resetTimer();
-        guessTimer = setInterval(function() {
+        guessTimer = setInterval(function () {
             timerLoop()
-            }, 1000);
+        }, 1000);
         $(".answers").empty()
 
         //tried creating an array so the questions are always in a different order. kept getting duplicates. this does too unfortunatly.
@@ -165,35 +154,25 @@ $(document).ready(function () {
         console.log(currentQuestion)
         $('.questions').text('Question: ' + currentQuestion.question)
         let questionAnswers = currentQuestion.answers
-
+        //grab answers from question array, create and place in divs
         for (var i = 0; i < currentQuestion.answers.length; i++) {
-
             let $validate = currentQuestion.answers[i].isCorrect
-            console.log(`name ${currentQuestion.answers[i].name}, validate ${$validate}`)
-            var displayAnswers = $('<div class="answers row"></div>')
-            var answer = questionAnswers[i]
-            var $newDivId = displayAnswers.append($(`<div class="btn col-12 mt-4" name="${currentQuestion.answers[i].name}" value="${$validate}">` + answer.name + `</div>`))
-            $('#imageHolder').append(currentQuestion.image)
+            let displayAnswers = $('<div class="answers row"></div>')
+            let answer = questionAnswers[i]
+            let $newDivId = displayAnswers.append($(`<div class="btn col-12 mt-4" name="${currentQuestion.answers[i].name}" value="${$validate}">` + answer.name + `</div>`))
             $('#questionsField').append($newDivId)
         }
-        
+
         $('.btn').on('click', function () {
             let guess = $(this).attr('value')
             if (guess > 0) {
                 console.log('Finally!')
                 userCorrect();
             } else {
-                on()
+                userWrong();
                 console.log('WRONG!')
-
-                function correct() {
-                    //highlight correct answer
-                    userWrong();
-                }
-                setTimeout(correct, 4000);
             }
         })
-        
     }
 
     function on() {
@@ -219,7 +198,7 @@ $(document).ready(function () {
     }
     //userCorrect function should hide other screens for a few seconds then load new game
     function userCorrect() {
-        
+
         resetTimer()
         $('#questionsField').hide()
         rightAnswers++
@@ -234,28 +213,33 @@ $(document).ready(function () {
         }
     }
     function correct() {
-                game()
-                $('#winScreen').hide()
-                $('#questionsField').show()
-            }
+        game()
+        $('#winScreen').hide()
+        $('#questionsField').show()
+    }
     //userWrongTime function will display correct answer wait a few seconds and show next question
     function userWrong() {
-
+        on()
         wrongAnswers++
         resetTimer()
         $('#wrongAnswers').text(wrongAnswers)
-        $('.answers').remove()
-        timerStop(guessTimer)
-        if (rightAnswers + wrongAnswers < questions.length) {
-            off()
-            game()
-        } else {
-            off()
-            statScreen()
-        }
-    }
-    //function to display user stats when all questions are answered with a restart button
 
+        timerStop(guessTimer)
+        function correct() {
+            //highlight correct answer
+            if (rightAnswers + wrongAnswers < questions.length) {
+                $('.answers').remove()
+                off()
+                game()
+            } else {
+                off()
+                statScreen()
+            }
+        }
+        setTimeout(correct, 4000);
+    }
+
+    //function to display user stats when all questions are answered with a restart button
     function statScreen() {
         resetTimer()
         timerStop(guessTimer)
@@ -268,9 +252,8 @@ $(document).ready(function () {
             newGame()
         })
     }
-function timerStop(t) {
-    clearInterval(t);
-    console.log(`Stop Timer`)
-};
-
+    function timerStop(t) {
+        clearInterval(t);
+        console.log(`Stop Timer`)
+    };
 })
